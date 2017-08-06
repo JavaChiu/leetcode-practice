be1d88f1f28bf64693c2e0a682770567c395c6f6
andrew <andrew@blackie>
2017-08-07 03:02:46 +0800

    8/7

--- src/medium/CountBits.java
+++ src/medium/CountBits.java
@@ -25,6 +25,16 @@ public class CountBits {
         return returnArray;
     }
     
+    /*
+     * online approach with bit manipulation 
+     * An easy recurrence for this problem is f[i] = f[i / 2] + i % 2.
+     */
+//    public int[] countBits(int num) {
+//        int[] f = new int[num + 1];
+//        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+//        return f;
+//    }
+    
     public static void main(String[] args) {
 		CountBits countBits = new CountBits();
 		int[] testArray = countBits.countBits(8);
